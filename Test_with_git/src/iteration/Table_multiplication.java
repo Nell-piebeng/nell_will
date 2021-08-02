27da0ab8aa7a2cfdac309c9f19b8a1ebd042f391
WilfriedNono90 <wilfried.nono90@gmail.com>
2021-08-02 19:43:02 +0200

    table de multiplication

---
+++ Test_with_git/src/iteration/Table_multiplication.java
@@ -0,0 +1,21 @@
+package iteration;
+
+import java.util.Scanner;
+
+public class Table_multiplication {
+
+	public static void main(String[] args) {
+		int resultat = 0;
+		int n ;
+		//on recupere n
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		
+		for (int i = 1 ; i<=100000 ; i++) {
+			resultat = n * i;
+			System.out.println(n+" * "+i+" = "+resultat);
+		}
+
+	}
+
+}
